81f935c0112f75da367f18b2983563cb32fd8a02
NikosGour <nikosgournak@gmail.com>
2023-01-23 18:50:11 +0200

    Latest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import { Route, Routes, Link } from 'react-router-dom';
 
+
+
 function App() {
   return (
     <div className="App">
-      {"Kiki's Delivery Service" + 2}
+      <p>
+        {"Kiki's Delivery Service" + 2}
+      </p>
     </div>
   );
 }
