4ae0f4733cbb17e10bff7f605b248252dc89b502
NikosGour <nikosgournak@gmail.com>
2023-01-23 21:56:04 +0200

    Latest

--- src/CarTransfer.js
+++ src/CarTransfer.js
@@ -8,7 +8,7 @@ const CarTransfer = () => {
             <h1 className="header">Car Transfer Page</h1>
             <h3 className="subheader">TRANSFER FOR {liscence_plate}</h3>
             <form className="vertical_form" action="/car_transfer">
-                <label for="transferAddress">Tranfer address:</label>
+                <label for="transferAddress">Transfer address:</label>
                 <input type="text" id="tranfer_address" name="transferAddress" />
                 <label for="afmTransferee">Afm of transferee:</label>
                 <input type="text" id="afm" name="afmTransferee" />
